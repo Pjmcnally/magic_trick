c44a904b2109a50f22625f5890e65dd53859f8c5
Patrick McNally <Patrick.j.mcnally@gmail.com>
2016-12-29 16:04:25 -0600

    update display function to properly space cards

--- trick.py
+++ trick.py
@@ -36,16 +36,21 @@ class Trick():
     print("""
     Left     Cent     Right
     ----     ----    ----
-    {l0}     {m0}    {r0}
-    {l1}     {m1}    {r1}
-    {l2}     {m2}    {r2}
-    {l3}     {m3}    {r3}
-    {l4}     {m4}    {r4}
-    {l5}     {m5}    {r5}
-    {l6}     {m6}    {r6}""".format(
-      l0=self.left[0], l1=self.left[1], l2=self.left[2], l3=self.left[3], l4=self.left[4], l5=self.left[5], l6=self.left[6],
-      m0=self.center[0], m1=self.center[1], m2=self.center[2], m3=self.center[3], m4=self.center[4], m5=self.center[5], m6=self.center[6],
-      r0=self.right[0], r1=self.right[1], r2=self.right[2], r3=self.right[3], r4=self.right[4], r5=self.right[5], r6=self.right[6],))
+    {:<4}     {:^4}    {:>4}
+    {:<4}     {:^4}    {:>4}
+    {:<4}     {:^4}    {:>4}
+    {:<4}     {:^4}    {:>4}
+    {:<4}     {:^4}    {:>4}
+    {:<4}     {:^4}    {:>4}
+    {:<4}     {:^4}    {:>4}""".format(
+      self.left[0], self.center[0], self.right[0],
+      self.left[1], self.center[1], self.right[1],
+      self.left[2], self.center[2], self.right[2],
+      self.left[3], self.center[3], self.right[3],
+      self.left[4], self.center[4], self.right[4],
+      self.left[5], self.center[5], self.right[5],
+      self.left[6], self.center[6], self.right[6])
+    )
   
   def get_row(self):
     if self.card:
